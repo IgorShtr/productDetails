c5d16594838df521c35cce58e936cdb425e9dfa7
Igor Shtr <igor.shtr@gmail.com>
2020-07-04 05:43:10 +0300

    Updates

---
+++ precache-manifest.f1f3b4d63ce732fc42107fc13fad8164.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ced0e6fd7ef3118d33cf219bb9b0da3",
+    "url": "/productdetails/index.html"
+  },
+  {
+    "revision": "39be6d52c2452c34a381",
+    "url": "/productdetails/static/css/main.fc76b3ca.chunk.css"
+  },
+  {
+    "revision": "bad6903d89ded63cb35d",
+    "url": "/productdetails/static/js/2.027a046e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/productdetails/static/js/2.027a046e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39be6d52c2452c34a381",
+    "url": "/productdetails/static/js/main.ca1cefb3.chunk.js"
+  },
+  {
+    "revision": "d556173eaf2987cd3dfe",
+    "url": "/productdetails/static/js/runtime-main.bc50e038.js"
+  },
+  {
+    "revision": "a6a18ae41917f808d73be8af581e62b3",
+    "url": "/productdetails/static/media/itemImg.a6a18ae4.jpg"
+  }
+]);
